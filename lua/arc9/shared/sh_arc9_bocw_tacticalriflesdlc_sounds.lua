8b021e7bd7769db58b08fdaa26ce8e5371bbcf53
multinett <72457258+multinettt@users.noreply.github.com>
2025-07-20 18:54:47 +0300

    Receive from main repo

---
+++ lua/arc9/shared/sh_arc9_bocw_tacticalriflesdlc_sounds.lua
@@ -0,0 +1,237 @@
+--=============================================================================
+--   ARC9 CALL OF DUTY: BLACK OPS COLD WAR
+--   TACTICAL RIFLES DLC
+--   SOUNDS MASTER FILE
+
+--========== CARV.2 ===========================================================
+-- MAGAZINE
+
+sound.Add({
+    name = "ARC9_BOCW.CARV2_reload_magout",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_carv2/carv2_reload_magout.wav"
+})
+
+sound.Add({
+    name = "ARC9_BOCW.CARV2_reload_magin",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_carv2/carv2_reload_magin.wav"
+})
+
+
+sound.Add({
+    name = "ARC9_BOCW.CARV2_reload_empty_magout",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_carv2/carv2_reload_empty_magout.wav"
+})
+
+sound.Add({
+    name = "ARC9_BOCW.CARV2_reload_empty_maginstart",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_carv2/carv2_reload_empty_maginstart.wav"
+})
+
+sound.Add({
+    name = "ARC9_BOCW.CARV2_reload_empty_magin",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_carv2/carv2_reload_empty_magin.wav"
+})
+
+
+
+sound.Add({
+    name = "ARC9_BOCW.CARV2_reload_ext_magout",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_carv2/carv2_reload_ext_magout.wav"
+})
+
+sound.Add({
+    name = "ARC9_BOCW.CARV2_reload_ext_magin",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_carv2/carv2_reload_ext_magin.wav"
+})
+
+
+sound.Add({
+    name = "ARC9_BOCW.CARV2_reload_ext_empty_magout",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_carv2/carv2_reload_ext_empty_magout.wav"
+})
+
+sound.Add({
+    name = "ARC9_BOCW.CARV2_reload_ext_empty_magin",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_carv2/carv2_reload_ext_empty_magin.wav"
+})
+
+-- CHARGING HANDLE
+
+sound.Add({
+    name = "ARC9_BOCW.CARV2_charginghandle_start",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_carv2/carv2_charginghandle_start.wav"
+})
+
+sound.Add({
+    name = "ARC9_BOCW.CARV2_charginghandle",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_carv2/carv2_charginghandle.wav"
+})
+
+-- RELOAD FOLEY
+
+sound.Add({
+    name = "ARC9_BOCW.CARV2_reload_start",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_carv2/carv2_reload_start.wav"
+})
+
+sound.Add({
+    name = "ARC9_BOCW.CARV2_reload_end",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_carv2/carv2_reload_end.wav"
+})
+
+
+sound.Add({
+    name = "ARC9_BOCW.CARV2_reload_empty_start",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_carv2/carv2_reload_empty_start.wav"
+})
+
+sound.Add({
+    name = "ARC9_BOCW.CARV2_reload_empty_end",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_carv2/carv2_reload_empty_end.wav"
+})
+
+-- INSPECT FOLEY
+
+sound.Add({
+    name = "ARC9_BOCW.CARV2_inspect_part1",
+    channel = CHAN_ITEM,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_carv2/carv2_inspect_part1.wav"
+})
+
+sound.Add({
+    name = "ARC9_BOCW.CARV2_inspect_part2",
+    channel = CHAN_ITEM,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_carv2/carv2_inspect_part2.wav"
+})
+
+sound.Add({
+    name = "ARC9_BOCW.CARV2_inspect_part3",
+    channel = CHAN_ITEM,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_carv2/carv2_inspect_part3.wav"
+})
+
+sound.Add({
+    name = "ARC9_BOCW.CARV2_inspect_part4",
+    channel = CHAN_ITEM,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_carv2/carv2_inspect_part4.wav"
+})
+
+sound.Add({
+    name = "ARC9_BOCW.CARV2_inspect_part5",
+    channel = CHAN_ITEM,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_carv2/carv2_inspect_part5.wav"
+})
+
+sound.Add({
+    name = "ARC9_BOCW.CARV2_inspect_part6",
+    channel = CHAN_ITEM,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_carv2/carv2_inspect_part6.wav"
+})
+
+sound.Add({
+    name = "ARC9_BOCW.CARV2_inspect_part7",
+    channel = CHAN_ITEM,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_carv2/carv2_inspect_part7.wav"
+})
+
+-- PULLOUT PUTAWAY
+
+sound.Add({
+    name = "ARC9_BOCW.CARV2_draw",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_carv2/carv2_draw.wav"
+})
+
+sound.Add({
+    name = "ARC9_BOCW.CARV2_holster",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_carv2/carv2_holster.wav"
+})
+
+
+sound.Add({
+    name = "ARC9_BOCW.CARV2_draw_quick",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_carv2/carv2_draw_quick.wav"
+})
+
+sound.Add({
+    name = "ARC9_BOCW.CARV2_holster_quick",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_carv2/carv2_holster_quick.wav"
+})
+
+-----------------------------------------------
+-- FIRING SOUNDS
+
+sound.Add({ -- BLACK OPS COLD WAR
+    name = "ARC9_BOCW.CARV2_fire",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    level = 100,
+    sound = "weapons/arc9/bocw_carv2/carv2_fire.wav"
+})
+
+sound.Add({
+    name = "ARC9_BOCW.CARV2_fire_silenced",
+    channel = CHAN_STATIC,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_carv2/carv2_fire_silenced.wav"
+})
+
+
+sound.Add({
+    name = "ARC9_BOCW.CARV2_fire_burst",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    level = 100,
+    sound = "weapons/arc9/bocw_carv2/carv2_fire_burst.wav"
+})
+
+sound.Add({
+    name = "ARC9_BOCW.CARV2_fire_silenced_burst",
+    channel = CHAN_WEAPON,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_carv2/carv2_fire_silenced_burst.wav"
+})
